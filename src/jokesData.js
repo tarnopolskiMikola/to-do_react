00f6c936b9af840e3e1c543fc5c49469cdb865f3
kola <email.com>
2022-03-18 18:03:00 +0200

    5-1

---
+++ src/jokesData.js
@@ -0,0 +1,15 @@
+const jokesData = [
+    
+    {
+        id: 1,
+        question: "What's the best thing about Switzerland?",
+        punchLine: "I don't know, but the flag is a big plus!"
+    },
+    {
+        id: 2,
+        question: "W zerland?",
+        punchLine: "I don't g plus!"
+    }
+]
+
+export default jokesData
